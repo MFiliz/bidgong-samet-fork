1b7718a8a096b5cfc3d07ca1ed4ba5a32011ee78
Samet İlhan <sametilhan@gmail.com>
2019-04-23 00:36:10 +0300

    sdaas

--- src/actions/GetCategoriesAction.js
+++ src/actions/GetCategoriesAction.js
@@ -7,7 +7,7 @@ export function getCategories(){
 		payload: new Promise((resolve, reject) => {
 			setTimeout(function() {
               resolve(
-                axios.get(`https://bidgong.herokuapp.com/[http://bidgongservices-dev.eu-central-1.elasticbeanstalk.com/api/Categories/GetAll]`,{headers: {'Access-Control-Allow-Origin': '*'}})
+                axios.get(`https://bidgong.herokuapp.com/http://bidgongservices-dev.eu-central-1.elasticbeanstalk.com/api/Categories/GetAll`,{headers: {'Access-Control-Allow-Origin': '*'}})
                 //   axios.get('http://bidgongservices-dev.eu-central-1.elasticbeanstalk.com/api/Categories/GetAll').then(res => res.data)
               )
 			}, 2000);

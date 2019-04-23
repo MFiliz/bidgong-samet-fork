2a9b44912c27a5437397ada11d4175d89b9e52eb
Samet İlhan <sametilhan@gmail.com>
2019-04-24 00:08:53 +0300

    asdasd

--- src/actions/GetCategoriesAction.js
+++ src/actions/GetCategoriesAction.js
@@ -6,10 +6,8 @@ export function getCategories(){
 		type: GET_CATEGORIES,
 		payload: new Promise((resolve, reject) => {
 			setTimeout(function() {
-              resolve(
-								resolve(axios.get('http://bidgongservices-dev.eu-central-1.elasticbeanstalk.com/api/Categories/GetAll')
-								.then(res => res.data.filter(data => data.isActive === true && data.isDeleted === false)))
-              )
+				resolve(axios.get('http://bidgongservices-dev.eu-central-1.elasticbeanstalk.com/api/Categories/GetAll')
+				.then(res => res.data.filter(data => data.isActive === true && data.isDeleted === false)))
 			}, 2000);
 		  })
 	}
